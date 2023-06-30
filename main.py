274a8b4ab5e27d1cea7949b94f8f322d0cbda124
65uto <94031819+65uto@users.noreply.github.com>
2023-06-30 17:33:49 +0700

    Update and rename slotGame.py to main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import random
 import customtkinter as ctk
 
 
+
 app = ctk.CTk()
 app.title("Slot")
 app.geometry("400x400")
@@ -10,21 +11,35 @@ ans = []
 i = 0
 s = []
 
+# ฟังก์ชั่นไว้ใช้สำหรับกดปุ่ม
+# เมื่อกดจะเปลียนค่า stop_label ตามลำดับตัวที่กด
 def click():
     global i
-    i += 1
-    if i > 3:
-        i = 0
-    text = "stop" + str(i)
-    #print(s[-1][0], s[-1][1], s[-1][2])
-    stop_label.configure(text=text)
-    
+    stop_b = stop_button.cget("text")
+    # ทำเงื่อนไขตรวจสอบข้อความบนปุ่ม
+    # ถ้าข้อความบนปุ่มเป็น Reset
+    # จะทำการเปลี่ยนข้อความบน stop_label เป็น stop0
+    # แล้วก็เปลี่ยนข้อความบน stop_button เป็น Start เพื่อใช้เป็นข้อความอ้างอิงในการตรวจสอบเงือนไข
+    if stop_b == "Reset":
+        text = "stop0"
+        stop_label.configure(text=text)
+        score_label.configure(text=" ")
+        stop_button.configure(text="Start")
+    elif stop_b == "stop" or stop_b == "Start": 
+        stop_button.configure(text="stop")
+        i += 1
+        if i > 3:
+            i = 0
+        text = "stop" + str(i)
+        #print(s[-1][0], s[-1][1], s[-1][2])
+        stop_label.configure(text=text)        
+
 def slot():
     global s
     number = [0,1,2,3,4,5,6,7,8,9,"@","#"]
-    stop = stop_label.cget("text")
+    stop = stop_label.cget("text") # ดึข้อความจาก stop_label เพื่อใช้อ้างอิง
 
-    if stop == "stop0":
+    if stop == "stop0": 
         random1 = random.choice(number)
         random2 = random.choice(number)
         random3 = random.choice(number)
@@ -41,10 +56,15 @@ def slot():
         random2 = s[-1][1]
         random3 = s[-1][2]
         if random1 and random2 == random3:
-            score_label.configure(text="Win")
+            score_label.configure(text="Win",text_color="#FFDE59")
+            stop_button.configure(text="Reset")
+            i=1
+            
         else:
             score_label.configure(text="Lose",text_color="#737373")
-
+            stop_button.configure(text="Reset")
+            i=1
+            
     text = [str(random1), str(random2), str(random3)]
     s.append(text)        
     label.configure(text=text)
